5ceda354d5b2a8a89bbd7044c661aaa4e2f611c3
David Andersson <lejonmanen@gmail.com>
2021-12-06 13:35:25 +0100

    Static mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div id="app">
+			<header>
+				<h1> Awesomesauce consulting </h1>
+				<nav className="nav">
+					<button> Welcome </button>
+					<button> Our vision </button>
+					<button> Contact us </button>
+				</nav>
+			</header>
+			<main> main </main>
+			<footer> footer </footer>
+		</div>
+	);
 }
 
 export default App;
